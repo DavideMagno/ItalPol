31187f94732c40850b679daf027a9e3f02ac05fe
Davide Magno <davide.magno@gmail.com>
2022-07-21 15:21:45 +0200

    Wrangle of all the polls to extract national electoral polls

--- WranglePolls.R
+++ WranglePolls.R
@@ -1,62 +1,103 @@
+GetColumnsFromStrings <- function(string) {
+  wrangled.text <- string |> 
+    stringr::str_split("\n") |> 
+    unlist()
+  
+  if (length(wrangled.text) > 1) {
+    wrangled.text <- wrangled.text |> 
+      tail(-1)
+  }
+  wrangled.text |> 
+    stringr::str_remove_all(stringr::fixed("movimento 5 stelle", 
+                                           ignore_case = TRUE)) |> 
+    stringr::str_remove_all(stringr::fixed("m5s", 
+                                           ignore_case = TRUE)) |> 
+    stringr::str_remove_all(stringr::fixed("articolo 1", 
+                                           ignore_case = TRUE)) |> 
+    stringr::str_remove_all(stringr::fixed("art1", 
+                                           ignore_case = TRUE)) |> 
+    stringr::str_extract_all("\\d+[(\\,\\d+)]*") |> 
+    purrr::map_dbl(length) |> 
+    unique() |> 
+    max()
+}
+
 WrangleAnswers <- function(domanda) {
   if (tibble::is_tibble(domanda$Risposta)) {
+
+    test.flag <- ifelse(ncol(domanda$Risposta) > 2, FALSE, TRUE)
     test <- domanda$Risposta |> 
       dplyr::pull("X1") |> 
       paste(collapse = "-")
   } else {
     test <- domanda$Risposta 
-  }
+    
+    number.variables <- GetColumnsFromStrings(test)
 
+    test.flag <- ifelse(number.variables > 1, FALSE, TRUE)
+  }
+  
   test <- grepl("\\bPartito Democratico\\b|\\bPD\\b", 
-         x = test, ignore.case = TRUE) & 
+                x = test, ignore.case = TRUE) & 
     grepl("\\bForza Italia\\b|\\bFI\\b", 
-         x = test, ignore.case = TRUE)
+          x = test, ignore.case = TRUE) &
+    !grepl("Elet", x = test, ignore.case = TRUE)
   
   if (test) {
-    test <- !grepl("fiducia|coalizion|gradimento", domanda$Testo, 
+    test <- !grepl("fiducia|coalizion|gradimento|elettor|simpatizzant|favorevole|legge|analisi|Draghi", 
+                   domanda$Testo, 
                    ignore.case = TRUE) 
   } else {
     test <- FALSE
   }
   
+  test <- test & test.flag
+  
   return(test)
 }
-
 
 FindElectoralPolls <- function(polls) {
   purrr::pluck(polls,  "Domande") |>
     purrr::map_lgl(WrangleAnswers)
 }
 
-a <- purrr::map(lista_nazionale, FindElectoralPolls)
+ExtractElectoralPoll <- function(poll, sondaggi.partiti) {
+  if(any(sondaggi.partiti)) {
+    domanda <- poll$Domande[min(which(sondaggi.partiti))] |> 
+      purrr::flatten()
+    poll$Domande <- NULL
+    poll <- c(poll, domanda)
+    
+    
+  } else {
+    poll <- NULL
+  }
+  return(poll)
+}
 
-purrr::map(a, sum)
+sondaggi.finali <- purrr::map2(lista_nazionale,
+                               sondaggi.partiti,
+                               ExtractElectoralPoll) |> 
+  {\(x) x[purrr::map_lgl(x, ~!is.null(.x))]}()
 
-# lista_nazionale[[1]] |>
-#   purrr::pluck("Domande") |>
-#   purrr::map(purrr::pluck("Risposta")) |>
-#   purrr::map_lgl(WrangleAnswers)
-# 
-# # 
-# # 
-# # 
+
 # ExtractNationalPolls <- function(polls) {
 #   index.nationals <- purrr::map_chr(polls,
 #                                     purrr::pluck("Estensione_Territoriale")) |>
 #     grep("italia|nazionale", x = _, ignore.case = TRUE)
-# 
+#   
 #   polls[index.nationals]
 # }
 # 
 # lista_nazionale <- ExtractNationalPolls(lista_sondaggi)
-# 
-# realizzatori <- purrr::map_chr(lista_nazionale, purrr::pluck("Realizzatore"))
-# 
-# lista_realizzatori <- realizzatori |> 
-#   unique()
-# 
-# lista_nazionale[realizzatori %in% lista_realizzatori[1]] |> 
-#   purrr::map(purrr::pluck("Domande")) |> 
-#   purrr::map(~purrr::map(.x, purrr::pluck("Testo"))) |> 
-#   purrr::flatten() |> 
-#   unlist()
+
+sondaggi.partiti <- purrr::map(lista_nazionale, FindElectoralPolls)
+
+purrr::map_dbl(sondaggi.partiti, sum)
+
+purrr::map_dbl(sondaggi.partiti, sum) |> 
+  {\(x) x[x >1]}()
+
+purrr::map_dbl(sondaggi.partiti, sum) |> 
+        {\(x) which(x == 3)}()
+
